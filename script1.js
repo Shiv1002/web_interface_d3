e2cdc81996a6dde72f7ca42fb8f7083c411bf1b0
Shiv1002 <85022318+Shiv1002@users.noreply.github.com>
2023-10-01 20:16:50 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -60,3 +60,66 @@ svg.append("g")
 svg.append("g")
     .attr("class", "y-axis")
     .call(d3.axisLeft(yScale));
+
+console.log('2 started')
+
+const radius = Math.min(width, height) / 2;
+
+// Create an SVG element
+const svg2 = d3.select("#graph2-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Create a group for the pie chart
+const g = svg2.append("g")
+    .attr("transform", `translate(${width / 2},${height / 2})`);
+
+// Define the color scale
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+// Define the pie layout
+const pie = d3.pie()
+    .value(d => d.sales);
+
+// Generate the pie chart slices
+const arcs = g.selectAll(".arc")
+    .data(pie(salesData))
+    .enter()
+    .append("g")
+    .attr("class", "arc");
+
+// Create the pie chart arcs
+arcs.append("path")
+    .attr("d", d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius)
+    )
+    .attr("fill", d => color(d.data.region));
+
+// Add labels for pie chart slices
+arcs.append("text")
+    .attr("transform", d => `translate(${d3.arc().centroid(d)})`)
+    .attr("text-anchor", "middle")
+    .text(d => d.data.region);
+
+// Add a legend
+const legend = svg2.selectAll(".legend")
+    .data(salesData.map(d => d.region))
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => `translate(0,${i * 20})`);
+
+legend.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .attr("fill", (d, i) => color(i));
+
+legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(d => d);
